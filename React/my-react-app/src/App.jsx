933907b954e4b631a9b3c5b4d60674ac7f4828fb
shre4ash <shreyashkumbhar9@outlook.com>
2025-06-13 21:08:52 +0530

    app components and imports

---
+++ React/my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header.jsx"
+import Footer from "./Footer.jsx"
+import Food from "./assets/Food.jsx";
+function App() {
+      
+  return(
+     //returning of two componats is not allowed
+    <>
+   <Header/>  
+   <Footer/>
+   <Food/>
+   </>  // we should inclose all componats in a fragments
+  );
+}
+
+export default App
